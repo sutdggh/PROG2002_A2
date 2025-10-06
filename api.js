225366dec1b46f7fa28d9a92cb971b5714d3b999
sutdggh <164153899+sutdggh@users.noreply.github.com>
2025-10-06 15:12:50 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const { getConnection } = require('./event_db');
+
+const app = express();
+app.use(express.json());
+
+// Import routes
+const eventsRouter = require('./routes/events');
+const categoriesRouter = require('./routes/categories');
+
+// Use routes
+app.use('/api/events', eventsRouter);
+app.use('/api/categories', categoriesRouter);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
